72a78916841edceddabc0628b8dc1804d8758b76
LuanBSilva <luanb.silva@unigranrio.br>
2020-10-12 18:19:26 -0300

    2.0

--- ProjetoPythonPCA Pense-Bem/ProjetoPCA Pt.2.py
+++ ProjetoPythonPCA Pense-Bem/ProjetoPCA Pt.2.py
@@ -4,123 +4,287 @@
 
 # Módulo
 from tkinter import *
+from random import randint
+
+# Variáveis Global
+
+
+qCount = 0
+usedQuestions = []
+actualAnswer = 0
+usersAnswer = 0
+points = 0
+
+def readFile():
+    # Função para Ler o Arquivo.txt
+
+    data = []   # 2d Matriz (Lista)
+    file = open("Questions.txt", mode = "r")
+
+    #iterar (loop), cada linha do arquivo e completar matriz
+    for line in file:
+        data.append(line.strip().split(','))
+
+    return data
+
+def displayQuestion():
+    # Questões Tela
+
+    data = readFile()
+
+    global qCount, usedQuestions, actualAnswer, points
+
+    # Gerador das Questões
+    randomQ = randint(0, len(data) - 1)
+    currentQuestion = str(data[randomQ][0])
+
+    while currentQuestion in usedQuestions and len(usedQuestions) < len(data):
+        randomQ = randint(0, len(data) - 1)
+        currentQuestion = str(data[randomQ][0])
+
+    if qCount < len(data):
+
+        # Display das Questões
+        resetBtns()
+        qNum.config(text = "Questão: {0}".format(qCount + 1))
+        question.config(text = data[randomQ][1])
+        btnA.config(text = data[randomQ][2])
+        btnB.config(text = data[randomQ][3])
+        btnC.config(text = data[randomQ][4])
+        btnD.config(text = data[randomQ][5])
+
+        qCount += 1
+        usedQuestions.append(currentQuestion)
+        actualAnswer = data[randomQ][6]
+    else:
+        gameOver.config(text = "Game Over\n\nPontuação Final: %s" % points)
+        gameOver.place(relwidth = 1, relheight = 1, relx = 0, rely = 0)
+        resetGame.place(relx = 0.32,
+                        rely = 0.85,
+                        relwidth = 0.35,
+                        relheight = 0.1)
+
+def checkAnswer():
+    # Resposta Selecionada e Aumento no número de pontos
+
+    global points
+
+    if int(actualAnswer) == usersAnswer:
+        points += 1
+        score.config(text = "Pontuação: %s" % points)
+    resetBtns()
+    displayQuestion()
+
+def resetgameBtn():
+
+    global qCount ,usedQuestions, actualAnswer, usersAnswer, points
+
+    qCount = 0
+    usedQuestions = []
+    actualAnswer = 0
+    usersAnswer = 0
+    points = 0
+    resetBtns()
+    displayQuestion()
+    removeGameover()
+
+def removeGameover():
+
+    gameOver.place(relwidth=1, relheight=1, relx = 1, rely = 1)
+    resetGame.place(relx=0.32,
+                    rely=1,
+                    relwidth=0.35,
+                    relheight=0.1)
+
+def btnPressed(answer):
+    # Botão selecionado
+
+    resetBtns()   # Função para resetar
+
+    if answer == 1:
+        btnA.config(fg = "#FFFFFF")
+    elif answer == 2:
+        btnB.config(fg = "#FFFFFF")
+    elif answer == 3:
+        btnC.config(fg = "#FFFFFF")
+    elif answer == 4:
+        btnD.config(fg = "#FFFFFF")
+
+    global usersAnswer
+    usersAnswer = answer
+
+def resetBtns():
+    # Resetar texto Botões
+
+    btnA.config(fg="#000000")
+    btnB.config(fg="#000000")
+    btnC.config(fg="#000000")
+    btnD.config(fg="#000000")
 
 def gui():
-    # Gráficos para o Programa
+# Gráficos para o Programa
+
+    global btnA, btnB, btnC, btnD, qNum, question, score, gameOver, resetGame
 
     # Configuração Básica de Janela
     win = Tk()  # Interface da Janela
     win.title("Pense-Bem")  # Título da Janela
     fgColour = "#FFFFFF"  # Cor da Fonte
     bgColour = "#000000"  # Cor de Fundo
+    text1 = ("Comic Sans MS", 25)
+    text3 = ("Comic Sans MS", 35)
+    text2 = ("Comic Sans MS", 20)
     text = ("Comic Sans MS", 18)
-    win.configure(bg = "#326fa8")  # Configuração da Janela
+    win.configure(bg="#326fa8")  # Configuração da Janela
     win.geometry("900x600")  # Dimensões da Janela
 
     # Quadro do Título
-    titleFrame = Frame(win, bg = "#000000")
-    titleFrame.place(relwidth = 1, relheight = 0.08)
+    titleFrame = Frame(win, bg="#000000")
+    titleFrame.place(relwidth=1, relheight=0.08)
 
     # Rótulo do Título
     Label(titleFrame,
-          text = "Pense-Bem",
-          font = text,
-          anchor = CENTER,
-          fg = fgColour,
-          bg = bgColour).place(relx = 0, relheight = 1)
+            text="Pense-Bem: Uma Aventura Científica com Sonic",
+            font=text,
+            anchor=CENTER,
+            fg=fgColour,
+            bg=bgColour).place(relx=0, relheight=1)
 
     # Rótulo da Pontuação
     score = Label(titleFrame,
-                  text = "Pontuação: 0",
-                  font = text,
-                  anchor = E,
-                  fg = fgColour,
-                  bg = bgColour)
+                    text="Pontuação: 0",
+                    font=text,
+                    anchor=E,
+                    fg=fgColour,
+                    bg=bgColour)
 
-    score.place(relx = 0.82,
-                relwidth = 0.18,
-                relheight= 1)
+    score.place(relx=0.82,
+                relwidth=0.18,
+                relheight=1)
 
     # Rótulo do Número do Programa
     qNum = Label(win,
-                 fg = fgColour,
-                 bg = bgColour,
-                 font = text)
+                fg=fgColour,
+                bg=bgColour,
+                font=text)
 
-    qNum.place(relx = 0.35,
-               rely = 0.20,
-               relwidth = 0.3,
-               relheight = 0.060)
+    qNum.place(relx=0.35,
+                rely=0.19,
+                relwidth=0.3,
+                relheight=0.050)
 
     # Rótulo da Questão
     question = Label(win,
-                 fg = fgColour,
-                 bg = "#303030",
-                 font = text)
+                    fg="#ff0000",
+                    bg="#303030",
+                    font=text3)
 
-    question.place(relx = 0.35,
-               rely = 0.25,
-               relwidth = 0.3,
-               relheight = 0.3)
+    question.place(relx=0.35,
+                rely=0.24,
+                relwidth=0.3,
+                relheight=0.3)
 
     # Botão Próximo
-    btnImg = PhotoImage(file = r"seta.gif")
+    btnImg = PhotoImage(file=r"seta.gif")
 
     nextBtn = Button(win,
-                     image = btnImg)
-
-    nextBtn.place(relx = 0.91,
-                  rely = 0.35,
-                  relwidth = 0.08,
-                  relheight = 0.1)
+                    image=btnImg,
+                     command = checkAnswer)
 
     # Botão Vermelho
     btnRed = PhotoImage(file=r"vermelho.gif")
 
     btnA = Button(win,
-                text = "Vermelho",
-                image = btnRed)
+                compound = CENTER,
+                font = text2,
+                text="A",
+                image=btnRed,
+                command=lambda: btnPressed(1))
 
-    btnA.place(relx = 0.00019,
-               rely = 0.6,
-               relwidth = 0.50,
-               relheight = 0.2)
+    btnA.place(relx=0.00019,
+                rely=0.6,
+                relwidth=0.50,
+                relheight=0.2)
 
     # Botão Amarelo
     btnYellow = PhotoImage(file=r"amarelo.gif")
 
     btnB = Button(win,
-                  text = "Amarelo",
-                  image = btnYellow)
+                compound = CENTER,
+                font = text2,
+                text="B",
+                image=btnYellow,
+                command=lambda: btnPressed(2))
 
-    btnB.place(relx = 0.50,
-               rely = 0.6,
-               relwidth = 0.50,
-               relheight = 0.2)
+    btnB.place(relx=0.50,
+                rely=0.6,
+                relwidth=0.50,
+                relheight=0.2)
 
     # Botão Azul
     btnBlue = PhotoImage(file=r"azul.gif")
 
     btnC = Button(win,
-                  text = "Azul",
-                  image = btnBlue)
+                compound = CENTER,
+                font = text2,
+                text="C",
+                image=btnBlue,
+                command=lambda: btnPressed(3))
 
-    btnC.place(relx = 0.00019,
-               rely = 0.8,
-               relwidth = 0.50,
-               relheight = 0.2)
+    btnC.place(relx=0.00019,
+                rely=0.8,
+                relwidth=0.50,
+                relheight=0.2)
 
     # Botão Verde
     btnGreen = PhotoImage(file=r"verde.gif")
 
     btnD = Button(win,
-                  text = "Verde",
-                  image = btnGreen)
+                compound = CENTER,
+                font = text2,
+                text="D",
+                image=btnGreen,
+                command=lambda: btnPressed(4))
 
-    btnD.place(relx = 0.50,
-               rely = 0.8,
-               relwidth = 0.50,
-               relheight = 0.2)
+    btnD.place(relx=0.50,
+                rely=0.8,
+                relwidth=0.50,
+                relheight=0.2)
+
+    # Função de destruir btns e Rodar Questões
+    def hideStart():
+        btnStart.destroy()
+        displayQuestion()
+
+        nextBtn.place(relx=0.91,
+                      rely=0.35,
+                      relwidth=0.08,
+                      relheight=0.1)
+
+    # Botão Começar
+    btnStart = Button(win,
+                      text = "Clique aqui para Começar",
+                      compound = CENTER,
+                      font = text1,
+                      bg = bgColour,
+                      fg = fgColour,
+                      command = hideStart)
+
+    btnStart.place(relx = 0.00001,
+                   rely = 0.6,
+                   relwidth = 1,
+                   relheight = 0.4,)
+
+    gameOver = Label(win,
+                     fg = "#FFFFFF",
+                     bg = "#000000",
+                     font = ("Comic Sans MS",50))
+
+    resetGame = Button(win,
+                       text = "Recomeçar",
+                       compound = CENTER,
+                       command = resetgameBtn,
+                       font = ("Comic Sans MS",20))
 
     win.mainloop()
 
